898344ec928a1c58734210246e46ce64f5560477
C Brosch <53014638+CBrosch0@users.noreply.github.com>
2021-11-06 13:57:41 -0400

    Gyro Turn

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy_AutoTemplate.java
@@ -0,0 +1,169 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import static java.lang.Math.abs;
+
+@Autonomous(name="Test Auto", group="Auto")
+
+public class FreightFrenzy_AutoTemplate extends LinearOpMode{
+    BNO055IMU imu;
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor Shooter = null;
+    private DcMotor Feeder = null;
+    private DcMotor Intake = null;
+
+    Orientation angles;
+
+    int inchesToEncoder(float inches){
+        final double     COUNTS_PER_MOTOR_REV    = 751.8 ;    // eg: TETRIX Motor Encoder
+        final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+        final double     WHEEL_DIAMETER_INCHES   = 3.93701 ;     // For figuring circumference
+        final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                (WHEEL_DIAMETER_INCHES * 3.1415);
+        int counts = (int) (COUNTS_PER_INCH * inches);
+        return(counts);
+    }
+
+    void resetMotors() {
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    void motorMove(){
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(frontLeftDrive.isBusy() || frontRightDrive.isBusy() || backLeftDrive.isBusy() || backRightDrive.isBusy()){
+            // Do Nothing
+        }
+    }
+
+    void noEncoder(){
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    void motorPower(float power){
+        frontLeftDrive.setPower(power);
+        frontRightDrive.setPower(power);
+        backLeftDrive.setPower(power);
+        backRightDrive.setPower(power);
+    }
+
+    void driveF(float power, int inches){ // Drive Forward
+        resetMotors();
+        motorPower(power);
+        int distance = inchesToEncoder(inches);
+        frontLeftDrive.setTargetPosition(distance);
+        frontRightDrive.setTargetPosition(distance);
+        backLeftDrive.setTargetPosition(distance);
+        backRightDrive.setTargetPosition(distance);
+        motorMove();
+    }
+
+    void driveR(float power, int degrees, String direction) { // Drive Rotate
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        float start = angles.firstAngle;
+        if(direction.equals("Right")){
+            power=-1*power;
+        }
+        noEncoder();
+        frontLeftDrive.setPower(-power);
+        frontRightDrive.setPower(power);
+        backLeftDrive.setPower(-power);
+        backRightDrive.setPower(power);
+
+        while(abs(angles.firstAngle)<degrees-.5 - start && opModeIsActive()){
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            telemetry.addLine().addData("heading", new Func<String>() {
+                @Override public String value() {
+                    return(toString().valueOf(angles.firstAngle));
+                }
+            });
+            telemetry.update();
+        }
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+    }
+
+    void driveRE(float power, int degrees, String direction) {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        if(direction.equals("Right")){
+            float position = angles.firstAngle - degrees;
+            if (abs(position)> 180){
+
+            }
+
+        }else if(direction.equals("Left")){
+            float position = angles.firstAngle + degrees;
+            if (abs(position)>180){
+
+            }
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        backLeftDrive = hardwareMap.get(DcMotor.class, "dtBL");
+        backRightDrive = hardwareMap.get(DcMotor.class, "dtBR");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "dtFL");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "dtFR");
+        Shooter = hardwareMap.get(DcMotor.class,"Shooter" );
+        Feeder = hardwareMap.get(DcMotor.class,"Feeder");
+        Intake = hardwareMap.get(DcMotor.class, "Intake");
+        frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        // Turn right
+        /*driveR(0.5f, 45);
+        sleep(1000);
+        driveR(0.5f, 90);
+        sleep(1000);
+        driveR(0.5f, 360);
+        sleep(1000);
+        driveR(1.0f, 90);*/
+
+        driveR(.5f, 90, "Left");
+        sleep(1000);
+        driveR(.5f, 135, "Right");
+        sleep(5000);
+    }
+}
+
